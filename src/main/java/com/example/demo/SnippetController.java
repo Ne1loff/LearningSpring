61e613e0882b327fdf4967a37d929836d2998f60
xinik <sergey20160@gmail.com>
2021-01-11 19:26:58 +0300

    Add code files endpoint and add new api methods

--- src/main/java/com/example/demo/SnippetController.java
+++ src/main/java/com/example/demo/SnippetController.java
@@ -37,7 +37,7 @@ public class SnippetController {
     }
 
     @PatchMapping("/snippets/{id}")
-    public Snippet changeSnippet (@RequestBody Snippet newSnippet, @PathVariable("id") long id) {
+    public Snippet changeSnippet(@RequestBody Snippet newSnippet, @PathVariable("id") long id) {
         Snippet oldSnippet = snippets.get(id);
         if (oldSnippet == null)
             throw new NotFoundException();
@@ -45,18 +45,91 @@ public class SnippetController {
         oldSnippet = newSnippet;
         oldSnippet.setId(mainId);
         snippets.replace(mainId, oldSnippet);
-        
+
         return oldSnippet;
     }
-    
+
     @DeleteMapping("/snippets/{id}")
-    public HttpStatus deleteSnippet (@PathVariable("id") long id) {
+    public HttpStatus deleteSnippet(@PathVariable("id") long id) {
         Snippet snippet = snippets.get(id);
         if (snippet == null)
             throw new NotFoundException();
         else {
             snippets.remove(snippet.getId());
             return HttpStatus.OK;
+        }
+    }
+
+    @GetMapping("/snippets/{snippetId}/{codeFileName}")
+    public CodeFile getCodeFile(@PathVariable("snippetId") long id, @PathVariable("codeFileName") String fileName) {
+        Snippet snippet = snippets.get(id);
+        if (snippet == null)
+            throw new NotFoundException();
+        else
+            return snippet.getContent().stream()
+                    .filter(it -> it.getName().equals(fileName))
+                    .findFirst()
+                    .orElseThrow(NotFoundException::new);
+    }
+
+    @PutMapping("/snippets/{snippetId}/{codeFileName}")
+    public HttpStatus putCodeFile(
+            @RequestBody CodeFile newCodeFile,
+            @PathVariable("snippetId") long id,
+            @PathVariable("codeFileName") String fileName
+    ) {
+        Snippet snippet = snippets.get(id);
+        if (snippet == null)
+            throw new NotFoundException();
+        else {
+            ArrayList<CodeFile> newContent = new ArrayList<>();
+            newCodeFile.setName(fileName);
+            newContent.add(newCodeFile);
+            snippet.setContent(newContent);
+            return HttpStatus.OK;
+        }
+    }
+
+    @PatchMapping("/snippets/{snippetId}/{codeFileName}")
+    public CodeFile changeCodeFile(
+            @RequestBody CodeFile newCodeFile,
+            @PathVariable("snippetId") long id,
+            @PathVariable("codeFileName") String fileName
+    ) {
+        Snippet snippet = snippets.get(id);
+        if (snippet == null)
+            throw new NotFoundException();
+        else {
+            ArrayList<CodeFile> changedContent = snippet.getContent();
+            newCodeFile.setName(fileName);
+            changedContent.replaceAll(it -> it.getName().equals(fileName) ? newCodeFile : it);
+            snippet.setContent(changedContent);
+            return newCodeFile;
+        }
+    }
+
+    @DeleteMapping("/snippets/{snippetId}/{codeFileName}")
+    public HttpStatus deleteCodeFile(
+            @PathVariable("snippetId") long id,
+            @PathVariable("codeFileName") String fileName
+    ) {
+        Snippet snippet = snippets.get(id);
+        if (snippet == null)
+            throw new NotFoundException();
+        else {
+            ArrayList<CodeFile> changedContent = snippet.getContent();
+
+            CodeFile removedCodeFile = changedContent.stream()
+                    .filter(it -> it.getName().equals(fileName))
+                    .findFirst()
+                    .orElseThrow(NotFoundException::new);
+
+            if (!changedContent.remove(removedCodeFile))
+                throw new NotFoundException();
+            else {
+                snippet.setContent(changedContent);
+                return HttpStatus.OK;
+            }
         }
     }
 }
