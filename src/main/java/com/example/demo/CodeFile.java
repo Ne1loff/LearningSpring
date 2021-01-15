78bcebca34a453c394d933bc6cd71a395545ab38
xinik <sergey20160@gmail.com>
2021-01-15 19:41:21 +0300

    Add changes by @sokket

--- src/main/java/com/example/demo/CodeFile.java
+++ src/main/java/com/example/demo/CodeFile.java
@@ -1,16 +1,22 @@
 package com.example.demo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.util.Date;
 
-@EqualsAndHashCode(callSuper = true)
 @Data
 @Entity
-public class CodeFile extends AuditModel {
+@JsonIgnoreProperties(
+        value = {"createdAt", "updatedAt"},
+        allowGetters = true
+)
+@EntityListeners(AuditingEntityListener.class)
+public class CodeFile {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
